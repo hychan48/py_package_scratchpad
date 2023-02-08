d3816fd6cc2980d9b9635010b78d53536065f78c
JC <jchan48@gmail.com>
2023-02-08 11:17:54 -0500

    added python env variables... should work need to figure out the syntax for testing on windows

--- dev/os_system_test.py
+++ dev/os_system_test.py
@@ -4,15 +4,36 @@ import pytest
 import logging as log
 
 import os
+
+# https://docs.pytest.org/en/7.1.x/how-to/skipping.html
+@pytest.mark.skipif(not os.name == 'nt' , reason="Window only")
 def test_is_windows():
     system_name = os.name
+    # also sys.platform
     # log.warning(system_name)
+    # log.warning(sys.platform)
+    # https: // docs.python.org / 3 / library / sys.html  # sys.platform
     assert system_name == 'nt'
+@pytest.mark.skipif(not os.name == 'nt' , reason="Window only")
+def test_windows_env():
+    # Local Python Env. seems to work
+    # might need to test for overriding
+    env_test = os.system('set ENV_TEST')
+    log.warning(env_test)
+    assert env_test == 1
+    # https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5#:~:text=To%20set%20and%20get%20environment%20variables%20in%20Python%20you%20can,Get%20environment%20variables%20USER%20%3D%20os.
+    os.environ['ENV_TEST'] = 'hello world'
+    # os.unsetenv()
+    env_test = os.system('set ENV_TEST')
+    assert env_test == 0
+    # log.warning(env_test)
 
+@pytest.mark.skipif(not os.name == 'posix' , reason="linux only")
 def test_is_ubuntu():
     system_name = os.name
-    # log.warning(system_name)
     assert system_name == 'posix'
+
+
 
 if __name__ == '__main__':
     pytest.main(sys.argv)
