02d32f719d6f8bff4198a1f24da496ad117608d2
JC <jchan48@gmail.com>
2023-03-06 22:48:44 -0500

    updated more test cases

--- yaml_modules/yaml_demo/directive_parser_target.py
+++ yaml_modules/yaml_demo/directive_parser_target.py
@@ -43,10 +43,28 @@ def directive_quote_at_at(string_target_input: str, at_at: str = "@@target"):
     return string_target_input.replace(at_at, f"'{at_at}'")  # replace all
 
 
+# 3. Remove Single from start and end { }
+def directive_extract_to_yaml_obj_str(string_target_input: str):
+    # quick and dirty - regex might be needed / triming
+    return string_target_input.replace("{", "", 1).replace("}", "", 1)
+    # return re.sub(r"[(^{)}]$","",string_target_input,2)
+    # return string_target_input.replace(at_at, f"'{at_at}'")  # replace all
+
+
 def directive_parser_target(string_target_input: str):
     # assuming string_target_input already semi-validated and @@target directive
     # input is from user. expect the unexpected
-    log.warning('hi')
+    # look at test_hard_coded_single_target_basic for more details
+
+    # 1. Replace Colon With Equals = to :. note needs space after colon
+    replace_colon_with_equals = directive_replace_equal_with_colon(string_target_input)
+    # 2. quote @@target
+    quote_at_at_target = directive_quote_at_at(replace_colon_with_equals)
+    # 3. Remove Single from start and end { }
+    extract_to_yaml_obj_str = directive_extract_to_yaml_obj_str(quote_at_at_target)
+    # 4. Run pyyaml safeloader
+    dict_target_directive = yaml.safe_load(extract_to_yaml_obj_str)
+    return dict_target_directive
 
 
 # --------------
@@ -62,8 +80,18 @@ class TestDirectiveReplaceEqualWithColon:
         ("hi= Hi", "hi: Hi"),
         ("start=1, end=2", "start: 1, end: 2"),  # multiple, still works
     ])
-    def test_capital_case_array(self, str_input, expected):
+    def test_directive_replace_equal_with_colon_multiple(self, str_input, expected):
         assert directive_replace_equal_with_colon(str_input) == expected
+
+
+class TestDirectiveExtractToYamlObjStr:
+    # to improve. only simple one is done
+    @pytest.mark.parametrize("str_input,expected", [
+        ("{{ }}", "{ }"),
+        # (" {{ }} ", "{ }"), # uncomment when ready. not sure if needed tbh yaml parser might do it
+    ])
+    def test_multi_directive_extract_to_yaml_obj_str(self, str_input, expected):
+        assert directive_extract_to_yaml_obj_str(str_input) == expected
 
 
 class TestDirectiveParserTarget:
@@ -84,18 +112,25 @@ class TestDirectiveParserTarget:
 
     def test_single_target_basic(self):
         s_input = "{{ @@target: guest-lnx, name-prefix=ub1, start=1, end=2, parallel=true, interval=5s, blocking=true }}"
-        # Pseudo Code
-        log.warning("hi")
+        out = directive_parser_target(s_input)
+        # log.warning(out)
+        assert out == {'@@target': 'guest-lnx', 'name-prefix': 'ub1', 'start': 1, 'end': 2, 'parallel': True,
+                       'interval': '5s', 'blocking': True}
 
     def test_multi_target_basic(self):
         s_input = "{{ @@target: [guest-lnx], name-prefix=ub1, start=1, end=2, parallel=true, interval=5s, blocking=true }}"
         # Pseudo Code
-        log.warning("hi")
+        out = directive_parser_target(s_input)
+        # log.warning(out)
+        assert out == {'@@target': ['guest-lnx'], 'name-prefix': 'ub1', 'start': 1, 'end': 2, 'parallel': True,
+                       'interval': '5s', 'blocking': True}
 
     # @pytest.mark.parametrize("str_input,expected", [
     #     ("hello", "Hello"),
     #     ("world", "World"),
     # ])
+    # def test_multi(self, str_input, expected):
+    #     assert directive_replace_equal_with_colon(str_input) == expected
 
 
 if __name__ == '__main__':
