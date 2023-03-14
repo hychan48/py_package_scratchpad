11af6a7faa111301c7d893038806836638d21398
JC <jchan48@gmail.com>
2023-03-14 15:45:49 -0400

    so something like this is how to split up the class

---
+++ dev/learn_class_methods/nested_class_trying.py
@@ -0,0 +1,59 @@
+import pytest
+import logging as log
+
+"""
+Base Class
+* States
+
+A uses States
+B uses States
+
+Combined Class
+C is both A and B
+* pretty sure this wont work
+
+"""
+
+
+class Base:
+    def __init__(self):
+        self.states = {
+
+        }
+        self.some_str = 'some_base'
+
+
+class A(Base):
+    def __init__(self):
+        # pass # will fail
+        super().__init__()  # need this  for it not to fail
+
+    def log_a(self):
+        log.warning("a :" + self.some_str)
+
+
+class B(Base):
+    def log_b(self):
+        log.warning("b :" + self.some_str)
+
+
+class C(A, B):
+    def log_c(self):
+        log.warning("c :" + self.some_str)
+
+    def __init__(self, someparam='s'):
+        super().__init__()  # actual syntax or A.__init__
+
+    #     super().__init__(self)
+    #     # default works?
+
+
+def test_main():
+    log.warning("test_main")
+    c = C()  # works.. but default __init is?
+    c.log_a()
+    c.log_b()
+
+    c.some_str = ' new some str '
+    c.log_a()
+    c.log_b()
