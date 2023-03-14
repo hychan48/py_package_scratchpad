ca097a393aaaabead6b05b5d98da4a7fdd2d2a57
JC <jchan48@gmail.com>
2023-03-14 01:32:47 -0400

    @classmethods aren't for generating factory functions... but are useful for initialize dynamic class properties etc.

--- dev/learn_class_methods/learn_class_methods.py
+++ dev/learn_class_methods/learn_class_methods.py
@@ -27,7 +27,7 @@ class CustomLogger:
 
     @classmethod
     # def init_metadata(cls, str_to_append:str = "CL: ", cl_to_append:str = 'blue'):
-    def init_metadata(cls, str_to_append: str = str_to_append, cl_to_append: str = cl_to_append):
+    def init_metadata(cls, str_to_append: str = None, cl_to_append: str = None):
         """
         fake metaclass?
         kinda lame... can't set variables like this
@@ -44,9 +44,9 @@ class CustomLogger:
         # return cls() # what's the difference?, this will crash though
         # return str(cs(cls.str_to_append,'blue'))
 
-    def __init__(self):
+    def __init__(self,cl_to_append=None,str_to_append=None):
         # these are class properties. so that's why there's no params in init
-        self.init_metadata(self.str_to_append, self.cl_to_append)
+        self.init_metadata(str_to_append, cl_to_append)
 
     @classmethod  # this works but kinda meaning less
     # guess i should be doing them inside __init__ instead like in init_metadata
@@ -60,7 +60,7 @@ class CustomLogger:
         """
         cls.cl_to_append = color
         cls.tmp = str(cs(cls.str_to_append, cls.cl_to_append))
-        return cls()
+        return cls
     # is this also more of a class method than anything? since it's static
     @classmethod
     def log(cls, s_input: str):
@@ -86,6 +86,11 @@ class WhiteInfoLogger(CustomLogger):
 # PyTest
 
 def test_name():
+    """
+    @classmethod does not produce factory functions as expected...
+    seems like it's just for storing static data amongst the classes
+    :return:
+    """
     cl = CustomLogger()
     cl.log('blue CL')
 
@@ -94,9 +99,40 @@ def test_name():
 
     cl_white = WhiteInfoLogger()
     cl_white.log('white wil')
-
+    cl_red.log('red CL')
+    cl.log('blue CL got changed...') # so it's not really a factory function
+    # it's really just static. but at least the init is nice?
     cls_meta = CustomLogger.init_metadata()()
-    cls_meta.log('blue CL')
+    cls_meta.log('blue CLS')
+    cl_red.log('red CL - init _metadata_broke red color') # init metadata broke it
+    # repeating to see if anything broke
+    cl.log('blue CL')
+    cl_red.log('red CL') # this broke... probably because i didnt call it right
+    cl_white.log('white wil')
+    cls_meta.log('blue CLS')
+
+def test_name_init_only():
+    cl = CustomLogger()
+    cl.log('blue CL')
+
+    cl_red = CustomLogger('red')
+    cl_red.log('red CL')
+
+    cl_white = WhiteInfoLogger()
+    cl_white.log('white wil')
+    cl_red.log('red CL')
+    cl.log('blue CL got changed...') # so it's not really a factory function
+    # it's really just static. but at least the init is nice?
+    cls_meta = CustomLogger('yellow')
+    cls_meta.log('yellow CLS')
+    cl_red.log('red CL - init _metadata_broke red color') # init metadata broke it
+    # repeating to see if anything broke
+    cl.log('blue CL')
+    cl_red.log('red CL') # this broke... probably because i didnt call it right
+    cl_white.log('white wil')
+    cls_meta.log('yellow CLS')
+
+
 
 
 if __name__ == '__main__':
